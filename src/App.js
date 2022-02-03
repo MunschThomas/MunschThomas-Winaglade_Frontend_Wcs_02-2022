a87c0c31912841f71b2efbd7e44b83776db5c41a
Martin Clara <clara-martin_student2021@wilder.school>
2022-02-03 19:15:09 +0100

    animation on bet page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Main from "./screens/Main";
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Main from './screens/Main'
+import Bet from './screens/Bet'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path='/' element={<Main />} />
+        <Route path='/Bet' element={<Bet />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
