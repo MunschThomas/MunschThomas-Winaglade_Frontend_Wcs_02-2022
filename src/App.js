f51028717da84fe7cd9ee3fd6e5fbadf741075ab
MUNSCH <thomas-munsch_student2021@wilder.school>
2022-02-03 15:05:30 +0100

    created a functional navbar, need css to be done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Main from "./screens/Main";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
