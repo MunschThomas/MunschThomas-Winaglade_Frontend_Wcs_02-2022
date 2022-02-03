b53656649fd1af835b4d62d7dd5677a158fbe55a
Yannick Lanteri <yannick-lanteri_student2021@wilder.school>
2022-02-03 15:03:44 +0100

    Create Home page

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,7 +1,23 @@
+import "./styles/Main.css";
+
 const Main = () => {
   return (
-    <div>
-      <h1>WINAGLADE</h1>
+    <div className="home">
+      <div className="homeIntro">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/81OOLRQvTSL.jpg"
+          alt="gladiator"
+        ></img>
+        <h1>AVE Casear, morituri te salutant</h1>
+      </div>
+      <div className="homeBestPlayers">
+        <h2>🏆 Gladiateur du mois🏆</h2>
+        <div className="homeBestPlayersCards">
+          <div className="homeBestPlayers1"></div>
+          <div className="homeBestPlayers2"></div>
+          <div className="homeBestPlayers3"></div>
+        </div>
+      </div>
     </div>
   );
 };
