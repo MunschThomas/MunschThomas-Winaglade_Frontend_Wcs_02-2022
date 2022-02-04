6d0cc56930089b92e9156fabee0123d681e77174
Martin Clara <clara-martin_student2021@wilder.school>
2022-02-04 05:53:36 +0100

    im of to bed, they still working on my branch, good luck boys

--- src/screens/Bet.js
+++ src/screens/Bet.js
@@ -7,16 +7,37 @@ const Bet = () => {
   const [isAnimate, setisAnimate] = useState(false)
   const [isAnimate2, setisAnimate2] = useState(false)
   const [isAnimate3, setisAnimate3] = useState(false)
+  const randomArr = []
+
+  const randomGladiators = () => {
+    while (randomArr.length < 6) {
+      const r = Math.floor(Math.random() * 20)
+      if (randomArr.indexOf(r) === -1) randomArr.push(r)
+    }
+  }
+  randomGladiators()
 
   return (
     <div className='wrapperBet'>
       <h1>Les paris</h1>
       <h2>Munus</h2>
-      <Duel isAnimate={isAnimate} setisAnimate={setisAnimate} />
+      <Duel
+        isAnimate={isAnimate}
+        setisAnimate={setisAnimate}
+        randomArr={randomArr}
+      />
       <h2>Harmatodromia</h2>
-      <Duel isAnimate={isAnimate2} setisAnimate={setisAnimate2} />
+      <Duel
+        isAnimate={isAnimate2}
+        setisAnimate={setisAnimate2}
+        randomArr={randomArr}
+      />
       <h2>Venatio</h2>
-      <Duel isAnimate={isAnimate3} setisAnimate={setisAnimate3} />
+      <Duel
+        isAnimate={isAnimate3}
+        setisAnimate={setisAnimate3}
+        randomArr={randomArr}
+      />
     </div>
   )
 }
